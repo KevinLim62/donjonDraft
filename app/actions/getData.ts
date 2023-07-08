87f50ea5c2e6d0689602e85c345b272ae520d79b
kevin.lim <kevin.lim@blockchainsg.com>
2023-07-08 15:23:40 +0800

    Changed of img src to local

--- app/actions/getData.ts
+++ app/actions/getData.ts
@@ -131,15 +131,15 @@ export const FooterContent = {
     title: 'Contact Us',
     socialMedia: [
         {
-            iconSrc:'/Linkedin',
+            iconSrc:`${baseURL}SocialMedia/linkedin.png`,
             content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
         {
-            iconSrc:'/Twitter',
+            iconSrc:`${baseURL}SocialMedia/twitter.png`,
             content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
         {
-            iconSrc:'/Facebook',
+            iconSrc:`${baseURL}SocialMedia/facebook.png`,
             content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
     ],
