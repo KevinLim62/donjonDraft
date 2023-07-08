091ed05654fb76020e25f0e481da9d2f29bca09a
kevin.lim <kevin.lim@blockchainsg.com>
2023-07-09 01:18:58 +0800

    Changed of layout size

--- app/components/Content/Intro.tsx
+++ app/components/Content/Intro.tsx
@@ -14,7 +14,7 @@ const Intro:React.FC<IntroProps> = ({
     bgSrc,
 }) => {
   return (
-  <section id='2' className="w-full relative container-box sm:h-[60vh] md:h-[80vh] lg:h-[90vh]">
+  <section id='2' className="w-full relative container-box h-full sm:h-[50vh] md:h-[55vh] lg:h-[70vh] xl:h-full">
       <Image
         alt="heroLogo"
         src={bgSrc}
@@ -28,7 +28,7 @@ const Intro:React.FC<IntroProps> = ({
         {title}
      </div>
     </div>
-      <div className="flex flex-col items-center justify-center h-full md:h-[40vh] lg:h-[58vh] xl:h-[60vh] pb-10">
+      <div className="flex flex-col items-center justify-center h-full sm:h-[35vh] md:h-[40vh] lg:h-[58vh] xl:h-[60vh] pb-10">
         <div className="content-box bg-gray-700 h-full py-10 sm:py-0 z-30">
           <div className="flex flex-col items-center justify-center h-full mx-5 md:mx-8 lg:mx-16 xl:mx-24">
               <div className="font-medium text-yellow-500 responsive-text text-center space-y-5">
@@ -40,14 +40,14 @@ const Intro:React.FC<IntroProps> = ({
               </div>
           </div>
         </div>
-        <div className="absolute w-[60px] h-[70px] sm:w-[80px] sm:h-[90px] lg:w-[120px] lg:h-[130px] xl:w-[160px] xl:h-[170px] 2xl:w-[220px] 2xl:h-[230px] left-[10%] top-[13%]">
+        <div className="absolute w-[60px] h-[70px] sm:w-[80px] sm:h-[90px] lg:w-[120px] lg:h-[130px] xl:w-[160px] xl:h-[170px] 2xl:w-[220px] 2xl:h-[230px] left-[10%] top-[10%] z-30">
         <Image
           alt="creatures"
           src="https://drive.google.com/uc?export=view&id=1vu8f1V4ByR4OIBXdokJyc_thTjgXInRd"
           fill
         />
       </div>
-      <div className="absolute w-[60px] h-[70px] sm:w-[80px] sm:h-[90px] lg:w-[120px] lg:h-[130px] xl:w-[160px] xl:h-[170px] 2xl:w-[220px] 2xl:h-[230px] right-[10%] bottom-[0%]">
+      <div className="absolute w-[60px] h-[70px] sm:w-[80px] sm:h-[90px] lg:w-[120px] lg:h-[130px] xl:w-[160px] xl:h-[170px] 2xl:w-[220px] 2xl:h-[230px] right-[10%] bottom-[0%] z-30">
         <Image
           alt="creatures"
           src="https://drive.google.com/uc?export=view&id=1So4BpJNNnavUdz_ry69aiTcfR-2tM3eh"
